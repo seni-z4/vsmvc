66b653fe70518c0837a0234938d80e94295f1809
seniya <seniyathri@gmail.com>
2024-07-19 15:21:20 +0200

    first commit

---
+++ src/main/java/com/example/vsmvc/VsmvcApplication.java
@@ -0,0 +1,13 @@
+package com.example.vsmvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VsmvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VsmvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/vsmvc/VsmvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vsmvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VsmvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
